e046bcb489c86276f4f7ff28163c91444e76cd2b
AnoopSharma <anoop647@gmail.com>
2023-02-18 02:15:08 +0530

    logged in user and token saved to user store

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,48 +1,43 @@
 import React, { useState } from "react";
 import { auth } from "../../firebase";
-import { toast, ToastContainer } from 'react-toastify'
+import { toast } from "react-toastify";
 
 const Register = () => {
-
-  const [email, setEmail] = useState('')
+  const [email, setEmail] = useState("");
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-
+    e.preventDefault();
+    // console.log("ENV --->", process.env.REACT_APP_REGISTER_REDIRECT_URL);
     const config = {
       url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
-      handleCodeInApp: true
-    }
+      handleCodeInApp: true,
+    };
 
-    await auth.sendSignInLinkToEmail(email, config)
-    toast.success(`
-    Email is sent to ${email}. Click the link to complete your registration`)
+    await auth.sendSignInLinkToEmail(email, config);
+    toast.success(
+      `Email is sent to ${email}. Click the link to complete your registration.`
+    );
+    // save user email in local storage
+    window.localStorage.setItem("emailForRegistration", email);
+    // clear state
+    setEmail("");
+  };
 
-    // save user to the local storage
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
 
-    window.localStorage.setItem('emailForRegistration', email)
-
-    //clear state
-
-    setEmail("")
-
-
-  }
-
-  const registerForm = () => <form onSubmit={handleSubmit}
-  >
-    <input
-      type="email"
-      className="form-control"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      autoFocus
-    />
-
-    <button type="submit" className="btn btn-raised">
-      Register
-    </button>
-  </form>
+      <button type="submit" className="btn btn-raised">
+        Register
+      </button>
+    </form>
+  );
 
   return (
     <div className="container p-5">
