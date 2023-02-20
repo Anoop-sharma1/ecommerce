4742eea8195c77fc5bd8611d5136d46030b141fa
AnoopSharma <anoop647@gmail.com>
2023-02-21 00:49:40 +0530

    added the redirects

--- client/src/pages/auth/ForgotPassword.js
+++ client/src/pages/auth/ForgotPassword.js
@@ -9,6 +9,14 @@ function ForgotPassword({ history }) {
     const [email, setEmail] = useState('');
     const [loading, setLoading] = useState(false);
 
+    const { user } = useSelector((state) => ({ ...state }));
+
+    useEffect(() => {
+        if (user && user.token) {
+            history.push('/');
+        }
+    }, [user]);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true)
